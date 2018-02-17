a49067da17d810f68f8f5d9777f50e82ac4c9ad6
Niraj Patel <nirajpatel@bbys-MacBook-Pro.local>
2018-02-17 13:08:05 -0500

    fixing the code for search bar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -5,7 +6,7 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetails from './components/video_detail';
 
-const API_KEY = "";
+const API_KEY = "AIzaSyBa9O5InZdtmsfc2Gq_asAvCM7YQtxJi9Q";
 
 
 class App extends Component  {
@@ -16,21 +17,30 @@ class App extends Component  {
       videos : [],
       selectedVideo: null
     };
-    YTSearch({key: API_KEY, term:'shape of you'}, (videos) => {
+
+    this.videoSearch("O mere dil k chain");
+
+
+
+
+
+
+  }
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos : videos,
         selectedVideo: videos[0]
       });
     });
-
-
-
   }
 
   render(){
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)},300);
     return (
           <div>
-            <SearchBar/>
+            <SearchBar onSearchTermChange={(term) => videoSearch(term)}/>
             <VideoDetails video={this.state.selectedVideo} />
             <VideoList
             onVideoSelect = { selectedVideo => this.setState({selectedVideo}) }
