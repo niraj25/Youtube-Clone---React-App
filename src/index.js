c50a7379acb53e6c09b7c82f86686c3c0a75b5a6
Niraj Patel <nirajpatel@bbys-MacBook-Pro.local>
2018-02-16 22:49:12 -0500

    added the initial version of the youtube

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetails from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = "AIzaSyBa9O5InZdtmsfc2Gq_asAvCM7YQtxJi9Q";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component  {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos : [],
+      selectedVideo: null
+    };
+    YTSearch({key: API_KEY, term:'shape of you'}, (videos) => {
+      this.setState({
+        videos : videos,
+        selectedVideo: videos[0]
+      });
+    });
+
+
+
+  }
+
+  render(){
+    return (
+          <div>
+            <SearchBar/>
+            <VideoDetails video={this.state.selectedVideo} />
+            <VideoList
+            onVideoSelect = { selectedVideo => this.setState({selectedVideo}) }
+            videos = {this.state.videos}/>
+         </div>
+       );
+  }
+
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
